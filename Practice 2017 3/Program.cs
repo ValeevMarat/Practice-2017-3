c26a522f81957fc4f2bef83e8baba7ede9cf2288
Marat <user@LAPTOP-7JKVIGGR>
2017-07-06 20:55:37 +0500

    Код написан и прокомментирован. Защита от "дурака" отсутствует

--- Practice 2017 3/Program.cs
+++ Practice 2017 3/Program.cs
@@ -10,6 +10,17 @@ namespace Practice_2017_3
     {
         static void Main(string[] args)
         {
-        }
+            Console.Write("Введите координату х: ");
+            double x = double.Parse(Console.ReadLine());                          // Последовательное чтение х и у
+
+            Console.Write("Введите координату y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            if (Math.Pow(x, 2) + Math.Pow(y - 1, 2) <= 1 & y <= 1 - Math.Pow(x, 2)) // Если точка принадлежит заданной области (см. задание в учебнике), то u=x-y, иначе u=xy+7
+                Console.WriteLine("u = "+(x-y));
+            else Console.WriteLine("u = " + (x*y+7));
+
+            Console.ReadLine();
+        } // Чтение точки и вывод u
     }
 }
