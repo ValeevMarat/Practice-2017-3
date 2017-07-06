e7591649e48937fee8c4afa5eaf3b6dc07a451f5
user <user@LAPTOP-7JKVIGGR>
2017-07-06 20:14:46 +0500

    Добавьте файлы проекта.

---
+++ Practice 2017 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Задание №3 практики 2017г. 
+// Задание 60в, стр. 22: Определить u, в зависимости от принадлежности точки к области
+// Ссылка на "Задачи по программированию": http://aesa.kz:8081/computer-science/abramov.pdf
+
+namespace Practice_2017_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
